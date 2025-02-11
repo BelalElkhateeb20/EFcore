9a6d832e48e6d4a26f7fc1ee9f8015b2f50a775c
Belal Elkhateeb <belalelmansy796@gmail.com>
2025-02-11 21:54:43 +0200

    Add project files.

---
+++ EFcore/ApplictionDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EFcore.Configrations;
+using EFcore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFcore
+{
+    public class ApplictionDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder option) =>
+             option.UseSqlServer(@"Data Source=(localdb)\ProjectModels;Initial Catalog=EFCore;Integrated Security=True");
+        protected override void OnModelCreating(ModelBuilder modelBuilder)//This for build the table in Database
+        {
+            new BlogEntityTypeConfigration().Configure(modelBuilder.Entity<Blog>());
+
+            //modelBuilder.ApplyConfigurationsFromAssembly(typeof(BlogEntityTypeConfigration).Assembly);// this Performs the same work 
+        }
+        public DbSet<Blog> Blogs { get; set; } 
+    }
+}
---
+++ EFcore/Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace EFcore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var _context = new ApplictionDbContext();
+
+            _context.SaveChanges();
+        }
+    }
+}
