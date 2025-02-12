b864f8b078d742f28eced6ac4f5686968665750a
Belal Elkhateeb <belalelmansy796@gmail.com>
2025-02-12 23:53:02 +0200

    /////

--- EFcore/Program.cs
+++ EFcore/Program.cs
@@ -7,25 +7,25 @@ namespace EFcore
     {
         static void Main(string[] args)
         {
-            var Blogs = new List<Blog>();
+            //var Blogs = new List<Blog>();
             var _context = new ApplictionDbContext();
-            var Blog = new Blog
-            {
-                Content = "Hello From Belal's Pc",
-                Title= "First Post",
-                url= "https://blog1.com/"
-            };
-            Blogs.Add(Blog);
-            Blog = new Blog
-            {
-                Content = "HR Department",
-                Title= "HR",
-                url= "https://blog1.com/"
-            };  
-            foreach (var blog in Blogs)
-            {
-                _context.Add(blog);
-            }
+            //var Blog = new Blog
+            //{
+            //    Content = "Hello From Belal's Pc",
+            //    Title= "First Post",
+            //    url= "https://blog1.com/"
+            //};
+            //Blogs.Add(Blog);
+            //Blog = new Blog
+            //{
+            //    Content = "HR Department",
+            //    Title= "HR",
+            //    url= "https://blog1.com/"
+            //};  
+            //foreach (var blog in Blogs)
+            //{
+            //    _context.Add(blog);
+            //}
 
             _context.SaveChanges();
         }
