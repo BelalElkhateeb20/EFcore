40b82699c7c4024341af378dd34ea65ac0f968aa
Belal Elkhateeb <belalelmansy796@gmail.com>
2025-02-12 23:57:58 +0200

    Add posts

--- EFcore/Migrations/20250212215737_AddPosts.Designer.cs
+++ EFcore/Migrations/20250212215737_AddPosts.Designer.cs
@@ -3,6 +3,7 @@ using EFcore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EFcore.Migrations
 {
     [DbContext(typeof(ApplictionDbContext))]
-    partial class ApplictionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250212215737_AddPosts")]
+    partial class AddPosts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- EFcore/Models/Post.cs
+++ EFcore/Models/Post.cs
@@ -3,20 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 namespace EFcore.Models
 {
-    public class Blog
+    public class Post
     {
-        public int Id { get; set; }
-        
-        public string url{get; set; }
+        public int PostId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
-
-        public List<Post> Posts { get; set; }
-
-
+        public int BlogId { get; set; }
     }
 }
